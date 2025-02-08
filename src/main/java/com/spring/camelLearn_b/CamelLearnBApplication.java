0931bf1e45b012973e37cbe8f6009881a03117c3
manishpandey <pandeym321@gmail.com>
2025-02-08 18:23:40 +0530

    Initial commit

---
+++ src/main/java/com/spring/camelLearn_b/CamelLearnBApplication.java
@@ -0,0 +1,13 @@
+package com.spring.camelLearn_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelLearnBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelLearnBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/camelLearn_b/CamelLearnBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.camelLearn_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelLearnBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
